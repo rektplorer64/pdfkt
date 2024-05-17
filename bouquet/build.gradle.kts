4ac3937d0e07a1b57e3094ab4a49e6ea1d2aec99
Wichit Pooh[ Wichit, Tanawin ] <tanawin.w@linecorp.com>
2024-05-17 15:14:11 +0700

    Upgraded libraries.

--- bouquet/build.gradle.kts
+++ bouquet/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     val composeVersion = rootProject.extra["compose_ui_version"]
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
